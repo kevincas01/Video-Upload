fbbad9f69221750846af568dc0d2bf7314992c02
Kevin Castro <kcapupp@gmail.com>
2024-01-06 00:16:29 -0500

    Use built in local storage lib

--- client/src/utils/localstorage.ts
+++ client/src/utils/localstorage.ts
@@ -1,23 +1,15 @@
 import { useEffect } from 'react';
 import localforage from 'localforage';
 
-export const  setLocalStorageData = async (key: string, value: any) => {
-  try {
-    await localforage.setItem(key, value);
-  } catch (error) {
-    console.error('Error setting data in local storage:', error);
-  }
+export const  setLocalStorageData = async (key: string, value: string) => {
+  
+  return localStorage.setItem(key, value)
 };
 
 export const  getLocalStorageData = async (key: string) => {
-  try {
-    const data = await localforage.getItem(key);
-    return data;
-  } catch (error) {
-    console.error('Error getting data from local storage:', error);
-  }
+  return localStorage.getItem(key)
 };
 
 export const clearLocalStorageData= ()=>{
-  localforage.clear()
+  localStorage.clear()
 }
