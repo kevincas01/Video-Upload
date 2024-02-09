de37ab9fc2627f082dc35529410df614a17bfff0
Kevin Castro <kcapupp@gmail.com>
2024-02-08 23:48:52 -0500

    make date utils

--- client/src/components/PreviewVideo.tsx
+++ client/src/components/PreviewVideo.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { getTimeDifference } from '../utils/date';
 
 interface PreviewVideoProps {
     videoid:number;
@@ -22,37 +23,9 @@ interface PreviewVideoProps {
 
     React.useEffect(() => {
         // Get the current date
-        const currentDate = new Date();
-        
-        console.log(currentDate)
-        // Calculate the time difference in milliseconds
-    const timeDifference = currentDate.getTime() - new Date(date).getTime();
-
-    // Calculate the time difference in minutes, hours, and days
-    const minutesAgo = Math.floor(timeDifference / (1000 * 60));
-    const hoursAgo = Math.floor(timeDifference / (1000 * 60 * 60));
-    const daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-
-    // Determine the time label based on the time difference
-    let timeLabel = "";
-    if (daysAgo >= 2) {
-        timeLabel = `${daysAgo} days ago`;
-    } else if (daysAgo === 1) {
-        timeLabel = "1 day ago";
-    } else if (hoursAgo >= 2) {
-        timeLabel = `${hoursAgo} hours ago`;
-    } else if (hoursAgo === 1) {
-        timeLabel = "1 hour ago";
-    } else if (minutesAgo >= 2) {
-        timeLabel = `${minutesAgo} minutes ago`;
-    } else if (minutesAgo === 1) {
-        timeLabel = "1 minute ago";
-    } else {
-        timeLabel = "Just now";
-    }
-
-    // Update the datePast state with the time label
-    setDatePast(timeLabel);
+        const timeLabel=getTimeDifference(date)
+        // Update the datePast state with the time label
+        setDatePast(timeLabel);
     }, []);
 
     
--- client/src/utils/localstorage.ts
+++ client/src/utils/localstorage.ts
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import localforage from 'localforage';
 
 export const  setLocalStorageData = async (key: string, value: string) => {
